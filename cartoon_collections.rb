6e919e541965307248e85fc5bea68518220f7421
Michelle Blevins <mjblevins7@gmail.com>
2018-03-24 19:15:22 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,10 @@ def long_planeteer_calls(array)
   end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |item|
+    item == cheese_types[0] || item == cheese_types[1] || cheese_types[2]
+  end
 end
